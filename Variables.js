418e3aa48ec92119f85df8cb4cc2c85b949c38f9
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-07-12 09:22:58 +0000

    learn about var , const, let

---
+++ Variables.js
@@ -0,0 +1,15 @@
+const account_id= 0 ; // const not change
+let account_email="lodhihariom@gmail.com"//changed,variables declare karne ke liye let ka use karte h
+var account_pass = 1234//changed,variables declare karne ke liye var ka use karte h
+city = "sagar"//bina type likhe variable declare kar sakte
+let accountstate;
+// account_id = 1//not allowed
+account_email = "hariom@gmail.com"
+account_pass = 1111
+city = "rahatgarh"
+console.log(account_id);
+console.table([account_email,account_pass,city,accountstate])
+/* 
+prefer not be use var
+because of issue in block scope and functional scope
+*/
