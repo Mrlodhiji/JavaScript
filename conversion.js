447c1ec5710992948d003e7eddcbb809ad48dbcb
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-07-12 09:55:33 +0000

    conversion of DataTypes

---
+++ conversion.js
@@ -0,0 +1,27 @@
+// let score = "33abc"
+// let score = null
+
+// console.log(typeof score)
+// console.log(typeof (score))
+// let value = Number(score)
+// console.log(typeof value)
+// console.log(value)
+
+// "33" = 33
+//"33and" = NaN
+//true = 1 ; false = 0
+// null = NaN
+
+
+// let x = 1;
+// let booleanX=Boolean(x)
+// console.log(booleanX)
+// 1 =true; 0 = false
+// "" = false
+//"hari"= true
+
+let s = 33
+let S = String(s)
+console.log(S)
+console.log(typeof S)
+
