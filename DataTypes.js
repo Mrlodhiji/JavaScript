f965d7d9e7872ffe38cee3a58cac8254c96b06b2
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-07-12 09:41:40 +0000

    Datatypes

---
+++ DataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+// alert("hello")// error,we are using node.js not browser
+let name = "hari"
+let age =19
+let isloggedIn = false
+//primitive
+// number = 2 to power 53
+//bigint
+//string = ""
+// boolean = true/false
+//null : standalone value / empty value
+// undefined = value not assign
+// symbol = unique
+
+//non primitive
+//object 
+
+console.log(typeof null)
+
