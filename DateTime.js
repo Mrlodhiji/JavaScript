fa3fc2026c509e512d646eda0967b6ce10da0cec
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-07-30 10:51:57 +0000

    Date and time basic in javaScript

---
+++ DateTime.js
@@ -0,0 +1,27 @@
+// Dates
+// let curDate = new Date();
+// console.log(curDate);
+// console.log(curDate.toDateString());
+// console.log(curDate.toLocaleDateString());
+// console.log(curDate.toLocaleString());
+// console.log(curDate.toString());
+// console.log(curDate.toLocaleTimeString());
+// console.log(curDate.toISOString());
+
+let myDate = new Date(2024,6,30);
+// console.log(myDate.toString())
+
+
+let timestamp =  Date.now();
+// console.log(Math.floor(Date.now()/1000));//seconds
+
+
+let NewDate = new Date()
+// console.log(NewDate.getDate())
+// console.log(NewDate.getDay().toString())
+// console.log(NewDate.getMonth())
+// console.log(NewDate.getFullYear())
+// console.log(NewDate.getHours())
+// console.log(NewDate.getMinutes())
+// console.log(NewDate.getSeconds())
+// console.log(NewDate.getMilliseconds())
