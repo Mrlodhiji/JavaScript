8ece671ccd5685d5aaf97554707ffa4526a3cc8c
Hariom Lodhi <155249241+Mrlodhiji@users.noreply.github.com>
2024-08-08 10:30:23 +0000

    Arrays and their methods

---
+++ Arrays.js
@@ -0,0 +1,27 @@
+const marvel_heros = ["thor","iroman","spiderman"];
+const dc_heros=["superman","flash","batman"];
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1])
+
+// const all = marvel_heros.concat(dc_heros)
+// console.log(all)
+
+// const allNew = [...marvel_heros,...dc_heros]//spred operator
+// console.log(allNew)
+
+// const arr = [1,2,3,[4,5],[6,7,[8,9]]] //flat funtion 
+// const realArr = arr.flat(Infinity);
+// console.log(realArr)
+
+// console.log(Array.isArray("hariom"))
+// console.log(Array.from("hariom"))
+// console.log(Array.from({name: "hari"}))
+
+// let s1 = 190;
+// let s2 = 200;
+// let s3 = 210;
+//  console.log(Array.of(s1,s2,s3))
+
+
